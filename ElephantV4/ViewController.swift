40352ddda5fe5880803048cd55a8f663599ea83e
Lionel Yu <lionel@musicalbasics.com>
2023-01-09 23:01:33 -0800

    Initial Commit

---
+++ ElephantV4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElephantV4
+//
+//  Created by Lionel Yu on 1/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
